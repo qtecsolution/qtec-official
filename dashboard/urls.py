0b72dd075f36d1095df7f3de16beca1b47bfe22b
shaiful <mug.shaiful97@gmail.com>
2022-06-16 20:20:06 +0600

    already_done added

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -8,7 +8,11 @@ app_name= 'dashboard'
 
 urlpatterns= [
     path('dashboard/', DashboardView.as_view(), name= 'dashboard_url'),
+
     path('already-done/', AlreadyDoneView.as_view(), name= 'already_done_url'),
+    path('already-done-delete/', AlreadyDoneView.as_view(), name= 'delet_what_done_url'),
+    path('already-done-save/', AlreadyDoneView.as_view(), name= 'save_what_done_url'),
+
     path('subscribe/', SubscribeView.as_view(), name= 'subscribe_url'),
     
 ]
--- dashboard/views/already_done_view.py
+++ dashboard/views/already_done_view.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
 
@@ -11,4 +12,16 @@ class AlreadyDoneView(View):
         context = {
             "what_Done" : what_Done,
         }
-        return render(request, 'already_done.html')
+        return render(request, 'already_done.html', context)
+    def post(self, request):
+        if request.resolver_match.url_name == "delet_what_done_url":
+            request_id = request.POST.get('id')
+            what_Done = WhatProjectHaveWeDone.objects.filter(id=request_id).delete()
+            return self.get(request)
+
+        if request.resolver_match.url_name == "save_what_done_url":
+            pass
+
+            
+
+        
