9984aa733cc1cd2bcb111079f5a515f59e5e1881
shaiful <mug.shaiful97@gmail.com>
2022-06-19 15:11:09 +0600

    already done edited

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from dashboard.views.already_done_view import AlreadyDoneView
+from dashboard.views.already_done_view import AlreadyDoneView, CaseStudyEditView
 
 from dashboard.views.dashboard import DashboardView
 from dashboard.views.subscribe_view import SubscribeView
+from dashboard.views.technologies_view import TechnologiesView
 
 app_name= 'dashboard'
 
@@ -11,8 +12,13 @@ urlpatterns= [
 
     path('already-done/', AlreadyDoneView.as_view(), name= 'already_done_url'),
     path('already-done-delete/', AlreadyDoneView.as_view(), name= 'delet_what_done_url'),
+    path('already-done-edit/', AlreadyDoneView.as_view(), name= 'edit_what_project_done_url'),
     path('already-done-save/', AlreadyDoneView.as_view(), name= 'save_what_done_url'),
+    path('case-study-edit/<int:id>/', CaseStudyEditView.as_view(), name= 'case_study_edit_url'),
 
     path('subscribe/', SubscribeView.as_view(), name= 'subscribe_url'),
-    
+
+    path('technologies/', TechnologiesView.as_view(), name= 'technologies_url'),
+    path('technologies-save/', TechnologiesView.as_view(), name= 'save_technologies_url'),
+    path('technologies-delete/', TechnologiesView.as_view(), name= 'delet_technologies_url'),    
 ]
