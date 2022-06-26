741beb5d3a3583a49a7d07904e03314d02ad263c
shaiful <mug.shaiful97@gmail.com>
2022-06-26 19:30:13 +0600

    error message added on application

--- homepage/views/index_view.py
+++ homepage/views/index_view.py
@@ -8,19 +8,18 @@ class IndexView(View):
 
     def get(self, request):
         project_done_all = WhatProjectHaveWeDone.objects.all()
-        project_done = project_done_all.values()
-        web_project = project_done.filter(project_type__icontains = '1')
-        print(web_project)
-        app_project = project_done.filter(project_type__icontains = '2')
-        print(app_project)
-        ui_ux = project_done.filter(project_type__icontains = '3')
+   
+        web_project = project_done_all.filter(project_type__icontains = '1')
+        app_project = project_done_all.filter(project_type__icontains = '2')
+        
+        ui_ux = project_done_all.filter(project_type__icontains = '3')
 
         blog = Blog.objects.order_by('-id')
         blog = blog[:4] if blog.count() > 4 else blog
         context = {
             'project_done_exists': project_done_all.exists(),
             'title': "Homepage",
-            'project_done': project_done,
+            'project_done': project_done_all,
             'web_project': web_project,
             'app_project': app_project,
             'ui_ux':ui_ux,
