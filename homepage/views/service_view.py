6734a9f734aa78f579abc004d5b877d48ec00c54
shaiful <mug.shaiful97@gmail.com>
2022-06-29 18:07:40 +0600

    service added

--- homepage/views/service_view.py
+++ homepage/views/service_view.py
@@ -13,7 +13,20 @@ class ServiceView(View):
 class ServiceDetailsView(View):
 
     def get(self, request, slug):
+        print("slug:::", slug)
         projects = ServiceDetailsProject.objects.filter(slug=slug)
 
         if slug == 'software-solutions-development':
             return render(request, 'service/software_solution_development.html', {'title': "Service Details", "projects": projects})
+        if slug == 'ui-ux-design':
+            return render(request, 'service/ui_ux_design.html', {'title': "UI UX Design", "projects": projects})
+        if slug == 'e-commerce-solutions':
+            return render(request, 'service/e_commerce_solutions.html', {'title': "E Commerce Solutions", "projects": projects})
+        if slug == 'mobile-application-development':
+            return render(request, 'service/mobile_application_development.html', {'title': "Mobile Application Development", "projects": projects})
+        if slug == 'lms-solutions':
+            return render(request, 'service/lms_solutions.html', {'title': "LMS Solutions", "projects": projects})
+        if slug == 'logistics-solutions':
+            return render(request, 'service/logistics_solutions.html', {'title': "Logistics Solutions", "projects": projects})
+															
+														
