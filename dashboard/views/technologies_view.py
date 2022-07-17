a41a1a5d06a7e457589e9bd5fa53e6fc643cfeb2
shaiful <mug.shaiful97@gmail.com>
2022-07-17 13:53:29 +0600

    bug fixed

--- dashboard/views/technologies_view.py
+++ dashboard/views/technologies_view.py
@@ -7,13 +7,22 @@ from django.contrib import messages
 
 
 class TechnologiesView(View):
-    def get(self, request):
+    def get(self, request, id=None):
         if request.resolver_match.url_name == "save_technologies_url":
             technology_features = TechnologyFeatures.objects.all()
             context = {
                 "technology_features" : technology_features,
             }
             return render(request, 'partial/technologies_create.html', context)
+        elif request.resolver_match.url_name == "update_technologies_url":
+            technology_features = TechnologyFeatures.objects.all()
+            technology = Technologies.objects.filter(id=id).first()
+            context = {
+                "technology_features" : technology_features,
+                "technology" : technology,
+            }
+            return render(request, 'partial/technologies_update.html', context)
+
         else:
             technologies = Technologies.objects.all()
             technology_features = TechnologyFeatures.objects.all()
@@ -23,7 +32,7 @@ class TechnologiesView(View):
             }
             return render(request, 'technologies.html', context)
         
-    def post(self, request):
+    def post(self, request,id=None):
         data = request.POST
         file = request.FILES
         if request.resolver_match.url_name == "save_technologies_url":
@@ -46,6 +55,43 @@ class TechnologiesView(View):
                 technologies.technology_features.add(*technology_features)
             messages.success(request, 'Data save successful!')
 
+            return redirect('dashboard:technologies_url')
+        if request.resolver_match.url_name == "update_technologies_url":
+            technologies = Technologies.objects.filter(id=id).first()
+            technologies.title = data.get('title')
+            technologies.description = data.get('description')
+
+            image = file.get('image')
+            if image:
+                technologies.image = file.get('image')
+
+            technologies.technology_details_title = data.get('technology_details_title')
+            technologies.technology_detail_description = data.get('technology_detail_description')
+
+            technology_detail_thumbnail = file.get('technology_detail_thumbnail')
+            if technology_detail_thumbnail:
+                technologies.technology_detail_thumbnail = technology_detail_thumbnail
+
+            technologies.why_this_technologies_title = data.get('why_this_technologies_title')
+            technologies.why_this_technologies_description = data.get('why_this_technologies_description')
+
+            why_this_technologies_image = file.get('why_this_technologies_image')
+            if why_this_technologies_image:
+                technologies.why_this_technologies_image = why_this_technologies_image
+
+            technologies.why_choice_title = data.get('why_choice_title')
+            technologies.why_choice_description = data.get('why_choice_description')
+
+            why_choice_image = file.get('why_choice_image')
+            if why_choice_image:
+                technologies.why_choice_image = why_choice_image
+
+            technologies.save()
+            technology_features = data.getlist('technology_features')
+            technologies.technology_features.clear()
+            if technology_features:
+                technologies.technology_features.add(*technology_features)
+            messages.success(request, 'Data Update successful!')
             return redirect('dashboard:technologies_url')
 
         if request.resolver_match.url_name == "delete_technologies_url":
