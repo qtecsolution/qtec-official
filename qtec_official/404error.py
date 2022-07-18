00b7f0e62db79df2ff893e1c41795284cb091ed0
shaiful <mug.shaiful97@gmail.com>
2022-07-18 17:56:04 +0600

    image size change

--- qtec_official/404error.py
+++ qtec_official/404error.py
@@ -4,9 +4,9 @@ from django.template import RequestContext
 
 
 def handler404(request, *args, **argv):
-    return render(request, '404error.html')
+    return render(request, '404error.html',{'title':'Error'})
 
 
 def handler500(request, *args, **argv):
-    return render(request, 'homepage/404error.html')
+    return render(request, 'homepage/404error.html', {'title':'Error'})
 
