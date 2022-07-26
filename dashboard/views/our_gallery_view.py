6673bab8f077496a08f03ce1a71915fb1695903b
shaiful <mug.shaiful97@gmail.com>
2022-07-26 19:06:24 +0600

    gallery add

---
+++ dashboard/views/our_gallery_view.py
@@ -0,0 +1,53 @@
+from django.shortcuts import redirect, render
+from django.views import View
+from django.contrib import messages
+from dashboard.models import OurGallery, Subscribe
+
+
+class OurGalleryView(View):
+    def get(self, request, id=None):
+        if request.resolver_match.url_name == "save_our_gallery_url":
+            return render(request, 'partial/our_gallery_create.html')
+        if request.resolver_match.url_name == "update_our_gallery_url":
+            our_gallery = OurGallery.objects.filter(id=id).first()
+            context = {
+                "our_gallery" : our_gallery,
+            }
+            return render(request, 'partial/update_gallery_create.html', context)
+        else:
+            our_gallery = OurGallery.objects.order_by('priority')
+            context = {
+                "our_gallery" : our_gallery,
+            }
+            return render(request, 'our_gallery.html', context)
+    def post(self, request, id=None):
+        data = request.POST
+        file = request.FILES
+        if request.resolver_match.url_name == "update_our_gallery_url":
+            gallery = OurGallery.objects.filter(id=id).first()
+            gallery.title = data.get('title')
+            gallery.description = data.get('description')
+            primary_image = file.get('primary_image')
+            if primary_image:
+                gallery.primary_image = primary_image
+            secondary_image = file.get('secondary_image')
+            if secondary_image:
+                gallery.secondary_image = secondary_image
+            gallery.priority = data.get('priority')
+            gallery.save()
+            messages.success(request, 'Data Update successful!')
+            return redirect('dashboard:our_gallery_url')
+        if request.resolver_match.url_name == "delete_our_gallery_url":
+            gallery = OurGallery.objects.filter(id=data.get('id')).first().delete()
+            messages.success(request, 'Data Deleted successful!')
+            return redirect('dashboard:our_gallery_url')
+        else:
+            gallery = OurGallery()
+            gallery.title = data.get('title')
+            gallery.description = data.get('description')
+            gallery.primary_image = file.get('primary_image')
+            gallery.secondary_image = file.get('secondary_image')
+            gallery.priority = data.get('priority')
+            gallery.save()
+            messages.success(request, 'Data save successful!')
+            return redirect('dashboard:our_gallery_url')
--- static/homepage/assets/js/sliders.js
+++ static/homepage/assets/js/sliders.js
@@ -1,34 +1,32 @@
 /**
- * 01. DORIES DIAMONDS SLIDER (3)
- *
+ * ABOUT US
+ * OUR GALLERY CAROUSEL
 **/
 
-// SLIDER START
-const swiper = new Swiper('.swiper', {
-    spaceBetween: 24,
-    slidesPerGroup: 1,
-    loop: true,
-    autoplay: {
-        delay: 4000,
-        disableOnInteraction: false,
-    },
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-        575: {
-            slidesPerView: 2
-        },
-
-        768: {
-            slidesPerView: 2
-        },
-
-        991: {
-            slidesPerView: 3
-        },
-    },
+var swiper = new Swiper(".mySwiper", {
+	direction: "vertical",
+	slidesPerView: 1,
+	mousewheel: false,
+	loop: true,
+	autoplay: {
+		delay: 4000,
+		disableOnInteraction: false,
+	},
+	
+	// Paginations bullet
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+		dynamicBullets: true,
+		type: "fraction",
+		renderBullet: function (index, className) {
+			return '<span class="' + className + '">' + (index + 1) + "</span>";
+		},
+	},
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
 });
