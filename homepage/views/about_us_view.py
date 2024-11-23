666eb408795d4f4a9a19ec8924017eef9dd417b2
biprajit-karmakar <resetmibot@gmail.com>
2024-11-23 18:39:12 +0600

    Add video section to about us page

--- homepage/views/about_us_view.py
+++ homepage/views/about_us_view.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 
-from dashboard.models import LegalTeamMembers, OurGallery, TeamMembers
+from dashboard.models import LegalTeamMembers, OurGallery, TeamMembers, YouTubeVideo
 
 
 class AboutUsView(View):
@@ -10,11 +10,14 @@ class AboutUsView(View):
         members = TeamMembers.objects.order_by('priority').values()
         legal_members = LegalTeamMembers.objects.order_by('priority').values()
         our_gallery = OurGallery.objects.order_by('priority').values()
+        video_link = YouTubeVideo.objects.filter(
+            page=YouTubeVideo.ABOUT_US, display=True).first()
         context = {
             "members": members,
             "legal_members": legal_members,
             "title": "About Us",
             "our_gallery": our_gallery,
+            "video_link": video_link,
 
         }
 
